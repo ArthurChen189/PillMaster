91be42dbb89e8fe7d56c89528e43e91e2caac779
HAODONG XU <h294xu@uwaterloo.ca>
2023-06-15 18:30:31 -0400

    fixed receiver screen reminder card, added muck data

--- app/src/main/java/com/ece452/pillmaster/screen/carereceiver/CareReceiverHomepageScreen.kt
+++ app/src/main/java/com/ece452/pillmaster/screen/carereceiver/CareReceiverHomepageScreen.kt
@@ -1,9 +1,11 @@
 package com.ece452.pillmaster.screen.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.rounded.DateRange
@@ -20,9 +23,15 @@ import androidx.compose.material.icons.rounded.Face
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,7 +40,11 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.ece452.pillmaster.R
 import com.ece452.pillmaster.utils.NavigationPath
 
 // Sample data of medicines
@@ -65,7 +78,8 @@ val medicines = listOf(
 @Composable
 fun CareReceiverHomepageScreen(
     // TODO - Expose an action if this action takes the user to another screen.
-    navController: NavController
+    navController: NavController,
+
 ) {
 
         Column(
@@ -79,8 +93,10 @@ fun CareReceiverHomepageScreen(
         ) {
             // TODO - Build this screen as per the Figma file.
             LazyColumn(modifier = Modifier.weight(1f)) {
-                items(medicines) { medicine ->
-                    Text(text = medicine, modifier = Modifier.padding(8.dp))
+                items(medicines){
+                        medicine ->
+                    //Text(text = medicine, modifier = Modifier.padding(8.dp))
+                    SingleReminderItem(medicine)
                 }
             }
             AddPillButton(navController)
@@ -135,22 +151,65 @@ fun AddPillButton(
     navController: NavController
 ){
     Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.End
+        modifier = Modifier.fillMaxWidth().padding(top = 0.dp),
     ) {
         Button(
             onClick = {
                 navController.navigate(NavigationPath.PILL_ADD_PAGE.route)
             },
-            Modifier.padding(horizontal = 15.dp)
+            Modifier.padding(horizontal = 5.dp).fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp)
         ) {
             Icon(
                 imageVector = Icons.Filled.Add,
                 contentDescription = "Add",
-                Modifier.size((40.dp))
+                Modifier.size((40.dp)),
+
             )
         }
     }
 }
 
+
+// please use under code to implement each pill reminder
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun SingleReminderItem (
+    reminder: String
+){
+    var isChecked by remember { mutableStateOf(false) }
+
+    Card(  shape = RoundedCornerShape(10.dp), modifier = Modifier
+        .padding(5.dp)
+        .fillMaxWidth(),
+    ) {
+        Row (modifier = Modifier.padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+
+            Image( modifier =  Modifier.size(50.dp),
+                painter = rememberImagePainter(R.drawable.ic_launcher_background),
+                contentDescription = null
+            )
+
+            Text(text = reminder, fontSize = 24.sp, modifier = Modifier.padding(start = 10.dp))
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Checkbox(
+                // below line we are setting
+                // the state of checkbox.
+                checked = isChecked,
+                // below line is use to add padding
+                // to our checkbox.
+                modifier = Modifier.padding(end = 5.dp),
+                // below line is use to add on check
+                // change to our checkbox.
+                onCheckedChange = { isChecked = it },
+
+            )
+        }
+    }
+
+}
 
